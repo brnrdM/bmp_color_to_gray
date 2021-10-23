ab104d9d52893cbe279d07f291aea60dac98de80
brnrdM <jrmorelos1999@gmail.com>
2021-10-23 14:44:41 -0400

    Fixed grayscale, added padding

--- lib/lib_bmp.h
+++ lib/lib_bmp.h
@@ -5,23 +5,24 @@
 #define DEBUG_PRINT(fmt, ...) \
     do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while(0)
 
-#define PIXEL_BITS_SIZE 32
+#define PIXEL_BITS_SIZE 24
+#define PIXEL_BYTE_SIZE 3
 
 typedef struct bmp_header {
     char BM[2];
-    unsigned int size_b;
+    unsigned int size_total_b;
     unsigned int offset_b;
     signed int width;
     signed int height;
     unsigned int bpp;
     unsigned int compress;
+    unsigned int size_bitmap;
 } bmp_header_t;
 
 typedef struct bmp_pixel {
-    unsigned char b;
-    unsigned char g;
     unsigned char r;
-    unsigned char alpha;
+    unsigned char g;
+    unsigned char b;
 } bmp_pixel_t;
 
 typedef struct bmp_img
@@ -33,9 +34,8 @@ typedef struct bmp_img
 int bmp_header_init(bmp_header_t *bmp_hdr, FILE *fp); 
 void bmp_pixel_init(bmp_pixel_t *bmp_pixel, 
                     unsigned char r, 
-                    unsigned char b,
                     unsigned char g,
-                    unsigned char alpha
+                    unsigned char b
                     );
 
 void bmp_print_header(bmp_header_t *bmp);
--- src/lib_bmp.c
+++ src/lib_bmp.c
@@ -26,7 +26,7 @@ bmp_header_init(bmp_header_t *bmp_hdr, FILE *fp)
     if (fread(&bmp_size, sizeof(bmp_size), 1, fp) != 1) {
         return 2;
     }
-    bmp_hdr->size_b = bmp_size;
+    bmp_hdr->size_total_b = bmp_size;
 
     /* Move Cursor 4 Bytes forward*/
     fseek(fp, 4, SEEK_CUR);
@@ -56,7 +56,7 @@ bmp_header_init(bmp_header_t *bmp_hdr, FILE *fp)
         return 2;
     }
 
-    if (bmp_hdr->bpp != 24) {
+    if (bmp_hdr->bpp != PIXEL_BITS_SIZE) {
         fprintf(stderr,"bpp of %u is currently unsupported.", bmp_hdr->bpp);
         return 2;
     }
@@ -72,6 +72,12 @@ bmp_header_init(bmp_header_t *bmp_hdr, FILE *fp)
         return 2;
     }
 
+    /* Read bitmap size */
+    fseek(fp, 34, SEEK_SET);
+    if (fread(&(bmp_hdr->size_bitmap), 4, 1, fp) != 1) {
+        return 2;
+    }
+
     rewind(fp);
 
     return 0;
@@ -82,28 +88,29 @@ bmp_print_header(bmp_header_t *bmp)
 {
     printf("BMP HEADER:\n");
     printf("HEAD: %s\n", bmp->BM);
-    printf("SIZE: %u\n", bmp->size_b);
+    printf("SIZE: %u\n", bmp->size_total_b);
     printf("OFFSET: %u\n", bmp->offset_b);
     printf("WIDTH: %u\n", bmp->width);
     printf("HEIGHT: %u\n", bmp->height);
     printf("BPP: %u\n", bmp->bpp);
     printf("COMPRESS: %u\n", bmp->compress);
+    printf("BITMAP_SIZE: %u\n", bmp->size_bitmap);
 }
 
 int
 bmp_pixel_read(int offset, int pixel_sz_bits, bmp_pixel_t *bmp_pixel, FILE *fp)
 {
     /* Assuming 24-bit AXRGB notation */
-    /* pixel bits are 4 bytes, 32 bits, or an int */
-    unsigned int bmp_pixel_bits;
+    /* pixel bits are 3 bytes, 24 bits */ 
+    unsigned int bmp_pixel_bits = 0;
     
-    if (pixel_sz_bits != 32)
+    if (pixel_sz_bits != PIXEL_BITS_SIZE)
         return 0;
     size_t ret;
 
     fseek(fp, offset, SEEK_SET);
 
-    if ((ret = fread(&bmp_pixel_bits, sizeof(bmp_pixel_bits), 1, fp)) != 1) {
+    if ((ret = fread(&bmp_pixel_bits, pixel_sz_bits/8, 1, fp)) != 1) {
         //fprintf(stderr,"Failed to read pixel\n");
         return 0;
     }
@@ -112,18 +119,15 @@ bmp_pixel_read(int offset, int pixel_sz_bits, bmp_pixel_t *bmp_pixel, FILE *fp)
     DEBUG_PRINT("offset: %d\n", offset);
     DEBUG_PRINT("ret: %ld\n", ret);
 
-    unsigned int a_mask = 0xFF000000;
-    unsigned int r_mask = 0x00FF0000;
-    unsigned int g_mask = 0x0000FF00;
-    unsigned int b_mask = 0x000000FF;
+    unsigned int r_mask = 0xFF0000;
+    unsigned int g_mask = 0x00FF00;
+    unsigned int b_mask = 0x0000FF;
 
-    bmp_pixel->alpha = (a_mask & bmp_pixel_bits) >> 24;
     bmp_pixel->r     = (r_mask & bmp_pixel_bits) >> 16;
     bmp_pixel->g     = (g_mask & bmp_pixel_bits) >> 8;
     bmp_pixel->b     = (b_mask & bmp_pixel_bits);
 
 
-    DEBUG_PRINT("A: %x %d\n", bmp_pixel->alpha, bmp_pixel->alpha);
     DEBUG_PRINT("R: %x %d\n", bmp_pixel->r, bmp_pixel->r);
     DEBUG_PRINT("G: %x %d\n", bmp_pixel->g, bmp_pixel->g);
     DEBUG_PRINT("B: %x %d\n", bmp_pixel->b, bmp_pixel->b);
@@ -132,17 +136,18 @@ bmp_pixel_read(int offset, int pixel_sz_bits, bmp_pixel_t *bmp_pixel, FILE *fp)
 }
 
 void
-bmp_pixel_init(bmp_pixel_t *bmp_pixel, unsigned char r, unsigned char b, unsigned char g, unsigned char alpha)
+bmp_pixel_init(bmp_pixel_t *bmp_pixel, unsigned char r, unsigned char g, unsigned char b)
 {
     bmp_pixel->b = b;
     bmp_pixel->g = g;
     bmp_pixel->r = r;
-    bmp_pixel->alpha = alpha;
 }
 
 void
 bmp_convert_pixel_to_gs(bmp_pixel_t *pxl)
 {
+    if ( (pxl->r == 255) && (pxl->b == 255) && (pxl->g == 255) )
+        return;
     /* NTSC Formula */
     unsigned char red = pxl->r * 0.299;
     unsigned char blu = pxl->b * 0.114;
@@ -156,8 +161,7 @@ bmp_convert_pixel_to_gs(bmp_pixel_t *pxl)
         pxl,
         biggest,
         biggest,
-        biggest,
-        pxl->alpha 
+        biggest
     );
 }
 
@@ -180,24 +184,55 @@ bmp_convert_to_gs(bmp_header_t *bmp, FILE *fp, FILE *fout)
     printf("\n");
 
     int pixel_array_size = bmp->width * bmp->height;
-    int pixel_array_end = pixel_array_size * 4  + bmp->offset_b; 
+    int pixel_array_end = bmp->offset_b + pixel_array_size * 4; 
     DEBUG_PRINT("ENDPXL: %i\n", pixel_array_end);
     bmp_pixel_t bmp_pixel;
     DEBUG_PRINT("SIZEOFPXL: %li\n", sizeof(bmp_pixel));
 
+    int bytes_in_pixel = bmp->bpp / 8;
+    int num_pixels_till_pad = 2;
+    unsigned short pad_bytes = 0;
+    unsigned long pixels_written = 0;
+
     /* Copy Pixel */
-    ret_p = bmp_pixel_read(bmp->offset_b2, PIXEL_BITS_SIZE, &bmp_pixel, fp);
+    ret_p = bmp_pixel_read(bmp->offset_b, bmp->bpp, &bmp_pixel, fp);
     ret_o = 1;
     while ((ret_p) && (index < pixel_array_end) && (ret_o)) {
-        bmp_convert_pixel_to_gs(&bmp_pixel);
-        DEBUG_PRINT("A: %x %d\n", bmp_pixel.alpha, bmp_pixel.alpha);
-        DEBUG_PRINT("R: %x %d\n", bmp_pixel.r, bmp_pixel.r);
-        DEBUG_PRINT("G: %x %d\n", bmp_pixel.g, bmp_pixel.g);
-        DEBUG_PRINT("B: %x %d\n", bmp_pixel.b, bmp_pixel.b);
-        ret_o = fwrite(&bmp_pixel, sizeof(bmp_pixel), 1, fout);
-        index += sizeof(bmp_pixel);
+        if (num_pixels_till_pad > 0) {
+            /* Write Converted Pixel */
+            bmp_convert_pixel_to_gs(&bmp_pixel);
+            DEBUG_PRINT("R: %x %d\n", bmp_pixel.r, bmp_pixel.r);
+            DEBUG_PRINT("G: %x %d\n", bmp_pixel.g, bmp_pixel.g);
+            DEBUG_PRINT("B: %x %d\n", bmp_pixel.b, bmp_pixel.b);
+            ret_o = fwrite(&bmp_pixel, bytes_in_pixel, 1, fout);
+            num_pixels_till_pad -= 1;
+            index += sizeof(bmp_pixel);
+            pixels_written += 1;
+        } else {
+            /* Write Padding */
+            ret = fread(&pad_bytes, sizeof(pad_bytes), 1, fp);
+            DEBUG_PRINT("PADDING %x\n", pad_bytes);
+            num_pixels_till_pad = 2;
+            ret_o = fwrite(&pad_bytes, sizeof(pad_bytes), 1, fout);
+            index += sizeof(pad_bytes);
+        }
         DEBUG_PRINT(" %ld ", index);
-        ret_p = bmp_pixel_read(index, PIXEL_BITS_SIZE, &bmp_pixel, fp);
+        if (pixels_written < pixel_array_size) {
+            ret_p = bmp_pixel_read(index, bmp->bpp, &bmp_pixel, fp);
+        } else {
+            break;
+        }
+    }
+
+    /* Copy End */
+    ret = fread(&byt, sizeof(byt), 1, fp);
+    DEBUG_PRINT("ret %li\n %li", ret, sizeof(byt));
+    ret_o = 1;
+    while ((ret) && (index < bmp->size_total_b) && (ret_o)) {
+        ret_o = fwrite(&byt, sizeof(byt), 1, fout);
+        index += ret_o;
+        DEBUG_PRINT(" %ld ", index);
+        ret = fread(&byt, sizeof(byt), 1, fp);
     }
 
 }
